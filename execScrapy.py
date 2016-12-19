b50f51c57fef94f3662fd69c2e90d6b0430ebdbb
zhoubo <congminghaoxue@foxmail.com>
2016-12-19 15:03:21 +0800

    add pcauto scrapy

--- execScrapy.py
+++ execScrapy.py
@@ -5,8 +5,8 @@ from scrapy.crawler import CrawlerProcess
 from spiders.spiders.bjanjuke import BjanjukeSpider
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-    #'LOG_LEVEL' : 'CRITICAL'
-    'LOG_LEVEL' : 'INFO'
+    'LOG_LEVEL' : 'CRITICAL'
+    # 'LOG_LEVEL' : 'INFO'
 })
 
 process.crawl(BjanjukeSpider)
