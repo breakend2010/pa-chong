08a909168e3f6041de679784a3be9285958efe87
zhoubo <congminghaoxue@outlook.com>
2017-09-01 18:05:31 +0800

    update execScrapy file for issf-data spider

--- execScrapy.py
+++ execScrapy.py
@@ -2,12 +2,13 @@
 # encoding: utf-8
 
 from scrapy.crawler import CrawlerProcess
-from spiders.spiders.bjanjuke import BjanjukeSpider
+
+from spiders.spiders.issf_data import IssfDataSpider
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-    'LOG_LEVEL' : 'CRITICAL'
-    # 'LOG_LEVEL' : 'INFO'
+    # 'LOG_LEVEL' : 'CRITICAL'
+    'LOG_LEVEL' : 'INFO'
 })
 
-process.crawl(BjanjukeSpider)
+process.crawl(IssfDataSpider)
 process.start()
