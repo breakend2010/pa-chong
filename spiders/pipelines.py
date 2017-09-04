bbf8b5f6bb1adc4e773c36325f2ebddb742178d9
zhoubo <congminghaoxue@outlook.com>
2017-09-04 18:07:49 +0800

    add issf-data's pipeline

--- spiders/pipelines.py
+++ spiders/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 # import mysql.connector as MySQLdb
-
+import json
 class SpidersPipeline(object):
     pass
     # def __init__(self):
@@ -37,3 +37,16 @@ class SpidersPipeline(object):
     #     except MySQLdb.Error as e:
     #         print("Something went wrong: {}".format(e))
     #     return item
+
+class JsonWriterPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('issf_data.json', 'a')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
