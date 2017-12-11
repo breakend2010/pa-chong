2103df58ec9f2fe7d965736574d76b6f8046ba69
zhoubo <congminghaoxue@outlook.com>
2017-12-11 17:57:38 +0800

    update bjanjuke spider

--- spiders/spiders/bjanjuke.py
+++ spiders/spiders/bjanjuke.py
@@ -2,6 +2,7 @@
 import scrapy
 from spiders.items import AnjukeSpidersItem
 
+
 class BjanjukeSpider(scrapy.Spider):
     name = "bjanjuke"
     allowed_domains = ["anjuke.com"]
@@ -13,11 +14,14 @@ class BjanjukeSpider(scrapy.Spider):
             'spiders.pipelines.SpidersPipeline': 400
         }
     }
+
     def parse(self, response):
-        houseList = response.selector.xpath('//div[contains(@class, "house-title")]/a/@href').extract()
+        houseList = response.selector.xpath(
+            '//div[contains(@class, "house-title")]/a/@href').extract()
         for houseUrl in houseList:
             yield scrapy.Request(houseUrl, callback=self.parseHouse)
-        nextPages = response.selector.xpath('//*[@id="content"]/div[5]/div[5]/a[7]/@href').extract()
+        nextPages = response.selector.xpath(
+            '//*[@id="content"]/div[5]/div[5]/a[7]/@href').extract()
         if nextPages:
             nextPage = response.urljoin(nextPages[0])
             print('page_url: ' + nextPage)
@@ -25,18 +29,32 @@ class BjanjukeSpider(scrapy.Spider):
 
     def parseHouse(self, response):
         item = AnjukeSpidersItem()
-        item['title'] = "".join(response.selector.xpath('//h3[@class="long-title"]/text()').extract())#[0].split())
-        item['arc'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[2]/dd/text()').extract())
-        item['composition'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[1]/dd/text()').extract()[0].split())
-        item['price'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[3]/dl[2]/dd/text()').extract()[0].split())
-        item['floor'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[4]/dd/text()').extract()[0].split())
-        item['location'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[2]/dd/p/a[2]/text()').extract()[0].split())
-        item['year'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[3]/dd/text()').extract()[0].split())
-        item['head_to'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[3]/dd/text()').extract()[0].split())
-        item['decoration'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[3]/dl[1]/dd/text()').extract()[0].split())
-        item['district'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[2]/dd/p/a[1]/text()').extract()[0].split())
-        item['block'] = "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[1]/dd/a[1]/text()').extract()[0].split())
+        item['title'] = "".join(response.selector.xpath(
+            '//h3[@class="long-title"]/text()').extract())  # [0].split())
+        item['arc'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[2]/dd/text()'
+        ).extract())
+        item['composition'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[1]/dd/text()').extract()[0].split())
+        item['price'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[3]/dl[2]/dd/text()').extract()[0].split())
+        item['floor'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[4]/dd/text()').extract()[0].split())
+        item['location'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[2]/dd/p/a[2]/text()').extract()[0].split())
+        item['year'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[3]/dd/text()').extract()[0].split())
+        item['head_to'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/dl[3]/dd/text()').extract()[0].split())
+        item['decoration'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[3]/dl[1]/dd/text()').extract()[0].split())
+        item['district'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[2]/dd/p/a[1]/text()').extract()[0].split())
+        item['block'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[1]/dd/a[1]/text()').extract()[0].split())
         item['link'] = response.url
-        item['created_at']="".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/h4/span/text()').extract()[0].split())
-        item['address']= "".join(response.selector.xpath('//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[2]/dd/p/text()[2]').extract()[0].split())
+        item['created_at'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/h4/span/text()').extract()[0].split())
+        item['address'] = "".join(response.selector.xpath(
+            '//*[@id="content"]/div[2]/div[1]/div[3]/div/div/div[1]/div[1]/dl[2]/dd/p/text()[2]').extract()[0].split())
         return item
