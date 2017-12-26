75a27f6747bc1a41b1cd4bb57fdfa2a5c2a074db
zhoubo <congminghaoxue@outlook.com>
2017-12-26 18:56:38 +0800

    split str by re

--- spiders/spiders/laoziliao.py
+++ spiders/spiders/laoziliao.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class LaoziliaoSpider(scrapy.Spider):
     name = "laoziliao"
     allowed_domains = ["www.laoziliao.net"]
-    start_urls = ['http://www.laoziliao.net/']
+    start_urls = ['http://www.laoziliao.net/rmrb/1949-02-06-1']
 
     def parse(self, response):
-        pass
+        '''//*[@id="33342"]
+        //*[@id="box"]/div[1]/div/div[1]
+        //*[@id="box"]/div[1]/div/div[2]
+        //*[@id="box"]/div[1]/div/div[1]
+        '''
+
+        resp = response.xpath('//*[@id="box"]/div[1]/div').extract()
+        print(re.compile(r'\W*(?=<h2)').split(resp[0])[3])
